e7a52060eef99f26d79bc4a8d1cd84aeb67ab578
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-02-04 14:03:02 -0500

    Este es mi primer commit local.

---
+++ eje_1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+  double anterior, actual;
+  anterior = actual = 0.;
+
+  for(int i=0; i<100; i++){
+    actual = sin(M_PI * 5 / 100 * i);
+  
+  }
+  return 0;
+}
+
