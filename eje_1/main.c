121cf1002cfd94a68e585307264e9d39a58377fb
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-02-05 13:48:46 -0500

    Terminando el ejemplo de cruces por cero.

--- eje_1/main.c
+++ eje_1/main.c
@@ -3,12 +3,15 @@
 
 
 int main(){
-  double anterior, actual;
+  double anterior, actual, constante;
   anterior = actual = 0.;
+  constante = 1.1 * M_PI * 5 / 100;
 
   for(int i=0; i<100; i++){
-    actual = sin(M_PI * 5 / 100 * i);
-  
+    actual = sin(constante * i);
+    if((anterior >= 0 && actual < 0 ) || (anterior < 0 && actual >= 0))
+      printf("Hay un cruce por cero en: %d\n",i);
+    anterior = actual;
   }
   return 0;
 }
