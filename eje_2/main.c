e3edbe6cfbe086019a73e49953985d1aee31434e
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-02-06 15:09:49 -0500

    Cuidado con el manejo de la memoria.

--- eje_2/main.c
+++ eje_2/main.c
@@ -14,6 +14,14 @@ int main(){
   *pNumber = 99;            // Assign a value to where the pointer is pointed to, NOT to the pointer variable
   std::cout << *pNumber << std::endl; // Print the new value "pointed to" by the pointer (99)
   std::cout << number << std::endl;   // The value of variable number changes as well (99)
+
+    // Peligro voy a mover a donde apunta el puntero
+  pNumber++;
+  std::cout << "Contenido siguente a number: " << *pNumber << std::endl;
+  pNumber++;
+  std::cout << "Contenido siguente a number: " << *pNumber << std::endl;
+  pNumber++;
+  std::cout << "Contenido siguente a number: " << *pNumber << std::endl;
   
   return 0;
 }
