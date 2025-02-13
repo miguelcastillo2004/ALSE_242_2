ebb46aa87861625fc140e0b151ca260f41714e7a
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-02-13 13:31:09 -0500

    Agregando un puntero a función.

--- eje3/funciones.cpp
+++ eje3/funciones.cpp
@@ -3,17 +3,23 @@
 
 
 double funeval2( const double &x ){
-   return sin(10*x) + cos(3*x);
+  // El puntero que permite llegar a esta función tiene la forma
+  // de double (*p)(const double&)
+   return sin(x/10) + cos(3*x/100);
 }
 
 double funeval( const double &x ){
+  // El puntero que permite llegar a esta función tiene la forma
+  // de double (*p)(const double&)
   return sin(M_PI*5*x/100);
 }
 
-void cruces_cero(const double &xl, const double &xu ){
+void cruces_cero( double (*f)(const double &), const double &xl, const double &xu ){
+  // El primer parámetro de esta función es un puntero a una función de la forma
+  // double (*p)(const double&)
   double anterior = 0., actual;  
   for(int i = xl; i<xu; i++){
-    actual = funeval( (double)i );
+    actual = f( (double)i );
     if((anterior >= 0 && actual < 0 ) || (anterior < 0 && actual >= 0))
       printf("Hay un cruce por cero en: %d\n",i);
     anterior = actual;
@@ -21,7 +27,8 @@ void cruces_cero(const double &xl, const double &xu ){
 }
 
 int main(){
-  cruces_cero(0, 100);
+  cruces_cero(funeval, 0, 100); // En este caso se llama la función Sin(M_PI*x/20)
+  cruces_cero(funeval2, 0, 100);// En este otro a sin(x/10)+cos(3*x/100)
   return 0;
 }
 
